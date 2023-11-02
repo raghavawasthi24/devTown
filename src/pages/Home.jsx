633a819a92e083bece093d599b0816daeabfb640
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-11-02 21:18:09 +0530

    initilized project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+const Home = () => {
+  return (
+    <div className='home'>
+       <div className="text-2xl font-bold text-white bg-orange-300 py-10 px-6 ">
+          Products
+       </div>
+          
+       <div>
+
+       </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/components/Button.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const button = styled.button`
+color:"red";
+`
